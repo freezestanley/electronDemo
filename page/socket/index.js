ffdc468f90512f89bca8ffd65d320c22bf678a82
chenzaining <chenzaining@zhongan.io>
2019-03-13 10:36:44 +0800

    修改debounce方法

--- page/socket/index.js
+++ page/socket/index.js
@@ -1,12 +1,12 @@
-function wsocket (urlValue) {
-  if (window.WebSocket) return (new window.WebSocket(urlValue));
-  if (window.MozWebSocket) return new MozWebSocket(urlValue);
+function wsocket(urlValue) {
+  if (window.WebSocket) return new window.WebSocket(urlValue)
+  if (window.MozWebSocket) return new MozWebSocket(urlValue)
   return false
 }
-function Wsocket (url) {
+function Wsocket(url) {
   this.url = url
   this.skt = wsocket(url)
-  this.skt.onopen = (ev) => {
+  this.skt.onopen = ev => {
     console.log('open')
     this.onopen(ev)
   }
@@ -14,25 +14,25 @@ function Wsocket (url) {
   this.skt.onclose = this.onclose
   this.skt.onerror = this.onerror
 }
-Wsocket.prototype.onopen = function (evt) {}
-Wsocket.prototype.onmessage = function (evt) {}
-Wsocket.prototype.onclose = function (evt) {}
-Wsocket.prototype.onerror = function (evt) {
+Wsocket.prototype.onopen = function(evt) {}
+Wsocket.prototype.onmessage = function(evt) {}
+Wsocket.prototype.onclose = function(evt) {}
+Wsocket.prototype.onerror = function(evt) {
   return new Error(evt)
 }
-Wsocket.prototype.send = function (param) {
+Wsocket.prototype.send = function(param) {
   if (this.skt.readyState === 1) {
     this.skt.send(param)
   } else if (this.skt.readyState === 3) {
     this.reconnect()
   }
 }
-Wsocket.prototype.close = function () {
+Wsocket.prototype.close = function() {
   this.skt.close()
 }
-Wsocket.prototype.reconnect = function () {
+Wsocket.prototype.reconnect = function() {
   this.skt = wsocket(this.url)
-  this.skt.onopen = (ev) => {
+  this.skt.onopen = ev => {
     console.log('open')
     this.onopen(ev)
   }
@@ -41,18 +41,24 @@ Wsocket.prototype.reconnect = function () {
   this.skt.onerror = this.onerror
 }
 
-export default Wsocket = Wsocket
+export default (Wsocket = Wsocket)
 
-export const debounce = function (method, delay) {
-  let timer = null
-  return function () {
-    let context = this, args = arguments
-    clearTimeout(timer)
-    timer = setTimeout(function(){
-      method.apply(context,args); 
+export const debounce = function(method, delay) {
+  return function() {
+    let context = this,
+      args = arguments
+    if (debounce.timer !== null) {
+      clearTimeout(debounce.timer)
+      debounce.timer = null
+    }
+    debounce.timer = setTimeout(function() {
+      method.apply(context, args)
+      clearTimeout(debounce.timer)
+      debounce.timer = null
     }, delay)
   }
 }
+debounce.timer = null
 
 // class debounceClass {
 //   constructor() {
@@ -70,21 +76,20 @@ export const debounce = function (method, delay) {
 //       let context = this, args = arguments
 //       clearTimeout(_this.time)
 //       _this.timer = setTimeout(function(){
-//         method.apply(context,args); 
+//         method.apply(context,args);
 //       }, delay)
 //     }
 //   }
 // }
 // export const debClass = new debounceClass()
 
-
-
-export const throttle = function (method, delay, time) {
-  let timeout, startTime = +new Date()
+export const throttle = function(method, delay, time) {
+  let timeout,
+    startTime = +new Date()
   return function() {
     let context = this,
-    args = arguments,
-    curTime = +new Date()
+      args = arguments,
+      curTime = +new Date()
     clearTimeout(timeout)
     if (curTime - startTime >= time) {
       method.apply(context, args)
